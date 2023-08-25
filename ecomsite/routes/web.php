564fcd645164db39db5d3bca835529556850bc19
Md.Razu Pramanik <44904498+RazuPk@users.noreply.github.com>
2023-08-25 17:33:30 +0600

    template mastering start

--- ecomsite/routes/web.php
+++ ecomsite/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,7 +23,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth','role:admin')->group(function () {
+    Route::controller(DashboardController::class)->group(function(){
+        Route::get('/admin/dashboard', 'Index');
+    });
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
